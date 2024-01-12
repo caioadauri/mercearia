3f3d40a5b12a9f8ee4f6469b2f9783143d73eaff
caioadauri <caioadaurigomes@gmail.com>
2024-01-12 09:50:53 -0300

    create method salver and ler for venda

--- DAO.py
+++ DAO.py
@@ -14,9 +14,34 @@ class DaoCategoria:
     
     cls.categoria = list(map(lambda x: x.replace('\n', ''), cls.categoria))
 
-    print(cls.categoria)
+    cat = []
+    for i in cls.categoria:
+      # print(i)
+      # Validar se o código abaixo está correto
+      cat.append(Categoria(i))
 
-# DaoCategoria.salvar('Frutas')
-# DaoCategoria.salvar('Legumes')
-# DaoCategoria.salvar('Verduras')
-DaoCategoria.ler()
+
+class DaoVenda:
+  @classmethod
+  def salvar(cls, venda: Venda):
+    with open('venda.txt', 'a') as arq:
+      arq.writelines(venda.itensVendido.nome + '|' + venda.itensVendido.preco + '|' + venda.itensVendido.categoria + '|' 
+                     + venda.vendedor + '|' + venda.comprador + '|' + str(venda.quantidadeVendida) + '|' + venda.data)
+      arq.writelines('\n')
+    
+  @classmethod
+  def ler(cls):
+    with open('venda.txt', 'r') as arq:
+      cls.venda = arq.readlines()
+      
+    cls.venda = list(map(lambda x: x.replace('\n', ''), cls.venda))
+    cls.venda = list(map(lambda x: x.split('|'), cls.venda))
+    vend = []
+    for i in cls.venda:
+      vend.append(Venda(Produtos(i[0], i[1], i[2]), i[3], i[4], i[5], i[6]))
+    return vend
+
+
+# DaoVenda.salvar(y)
+x = DaoVenda.ler()
+print(x)
