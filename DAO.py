2724f46231b34643a389045a2830fe0d09b50af6
caioadauri <caioadaurigomes@gmail.com>
2024-01-12 16:04:22 -0300

    create method salvar and ler for estoque and fornecedor

--- DAO.py
+++ DAO.py
@@ -2,7 +2,7 @@ from Models import *
 
 class DaoCategoria:
   @classmethod
-  def salvar(cls, categoria):
+  def salvar(cls, categoria : Categoria):
     with open('categoria.txt', 'a') as arq:
       arq.writelines(categoria)
       arq.writelines('\n')
@@ -42,6 +42,41 @@ class DaoVenda:
     return vend
 
 
-# DaoVenda.salvar(y)
-x = DaoVenda.ler()
-print(x)
+class DaoEstoque:
+  @classmethod
+  def salvar(cls, produto: Produtos, quantidade):
+    with open('estoque.txt', 'a') as arq:
+      arq.writelines(produto.nome + '|' + produto.preco + '|' + produto.categoria + '|' + str(quantidade))
+      arq.writelines('\n')
+  
+  @classmethod
+  def ler(cls):
+    with open('estoque.txt', 'r') as arq:
+      cls.estoque = arq.readlines()
+    
+    cls.estoque = list(map(lambda x: x.replace('\n', ''), cls.estoque))
+    cls.estoque = list(map(lambda x: x.split('|'), cls.estoque))
+    estoq = []
+    for i in cls.estoque:
+      estoq.append(Estoque(Produtos(i[0], i[1], i[2], i[3])))
+    return estoq
+  
+
+class DaoFornecedor:
+  @classmethod
+  def salvar(cls, fornecedor: Fornecedor):
+    with open('fornecedor.txt', 'a') as arq:
+      arq.writelines(fornecedor.nome + '|' + fornecedor.cnpj + '|' + fornecedor.telefone + '|' + fornecedor.categoria)
+      arq.writelines('\n')
+  
+  @classmethod
+  def ler(cls):
+    with open('fornecedor.txt', 'r') as arq:
+      cls.fornecedores = arq.readlines()
+
+    cls.fornecedores = list(map(lambda x: x.replace('\n', ''), cls.fornecedores))
+    cls.fornecedores = list(map(lambda x: x.split('|'), cls.fornecedores))
+    forn = []
+    for i in cls.fornecedores:
+      forn.append(Fornecedor(i[0], i[1], i[2], i[3]))
+    return forn
