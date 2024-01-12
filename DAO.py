abfda5c7287c34aad7e05a5d6de21859aa4cfaa4
caioadauri <caioadaurigomes@gmail.com>
2024-01-11 21:54:24 -0300

    create method salver and ler

--- DAO.py
+++ DAO.py
@@ -1,2 +1,22 @@
 from Models import *
 
+class DaoCategoria:
+  @classmethod
+  def salvar(cls, categoria):
+    with open('categoria.txt', 'a') as arq:
+      arq.writelines(categoria)
+      arq.writelines('\n')
+  
+  @classmethod
+  def ler(cls):
+    with open('categoria.txt', 'r') as arq:
+      cls.categoria = arq.readlines()
+    
+    cls.categoria = list(map(lambda x: x.replace('\n', ''), cls.categoria))
+
+    print(cls.categoria)
+
+# DaoCategoria.salvar('Frutas')
+# DaoCategoria.salvar('Legumes')
+# DaoCategoria.salvar('Verduras')
+DaoCategoria.ler()
