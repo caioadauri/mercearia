b37589a8fad5e2a5b28a39fd4e9c793df9946238
caioadauri <caioadaurigomes@gmail.com>
2024-01-16 10:22:21 -0300

    alterar categoria

--- controller.py
+++ controller.py
@@ -33,3 +33,25 @@ class ControllerCategoria:
         for i in x:
           arq.writelines(i.categoria)
           arq.writelines('\n')
+
+  def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
+    x = DaoCategoria.ler()
+    cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
+
+    if len(cat) > 0:
+      cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
+      if len(cat1) == 0:
+        x = list(map(lambda x: Categoria(categoriaAlterada) if (x.categoria == categoriaAlterar) else(x), x))
+        print('Categoria alterada com sucesso!')
+      else:
+        print('Categoria que dejesa alterar já existe')
+    else:
+      print('Categoria que deseja alterar não existe')
+
+    with open('categoria.txt', 'w') as arq:
+      for i in x:
+        arq.writelines(i.categoria)
+        arq.writelines('\n')
+
+a = ControllerCategoria()
+a.alterarCategoria('Carnes', 'Frutas')
